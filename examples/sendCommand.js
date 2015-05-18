1ebe8a1d7dd091900f840c77798fb3dd975a069e
Manuel Alabor <msites@msites.net>
2015-05-18 18:25:47 +0200

    Bugfixes, Commands without Responses and more Examples
    * This commit fixes a bug where the `turnOff`-promise was never resolved since
      the activityId passed to `startActivity` was a Number instead of a String
    * `send` and `request` allow to communicate on a request/response basis or with
      "fire and forget".
    * The `examples` directory contains a new example to show how one could send
      device specific actions to the hub.

---
+++ examples/sendCommand.js
@@ -0,0 +1,30 @@
+var HarmonyHubClient = require('../index');
+
+HarmonyHubClient('192.168.1.12')
+	.then(function(harmonyClient) {
+		return harmonyClient.getAvailableCommands()
+			.then(function(commands) {
+				// Look for the first device and pick its "power" control group, pick
+				// there the "poweron" function and trigger it:
+				var device = commands.device[0]
+					, powerControls = device.controlGroup
+						.filter(function(group) { return group.name.toLowerCase() === 'power'; })
+						.pop()
+					, powerOnFunction = powerControls['function']
+						.filter(function(action) { return action.name.toLowerCase() === 'poweron'; })
+						.pop();
+
+				if(powerOnFunction) {
+					var encodedAction = powerOnFunction.action.replace(/\:/g, '::');
+					return harmonyClient.send('holdAction', 'action=' + encodedAction + ':status=press');
+				} else {
+					throw new Error('could not find poweron function of first device :(');
+				}
+			})
+			.finally(function() {
+				harmonyClient.end();
+			});
+	})
+	.catch(function(e) {
+		console.log(e);
+	});
--- examples/toggleTv.js
+++ examples/toggleTv.js
@@ -1,30 +1,35 @@
 var HarmonyHubClient = require('../index');
 
-HarmonyHubClient('192.168.1.26')
-.then(function(harmonyClient) {
-	harmonyClient.isOff()
-	.then(function(off) {
-		if(off) {
-			console.log('Currently off. Turning TV on.');
+HarmonyHubClient('192.168.1.11')
+	.then(function(harmonyClient) {
+		return harmonyClient.isOff()
+			.then(function(off) {
+				if(off) {
+					console.log('Currently off. Turning TV on.');
 
-			harmonyClient.getActivities()
-			.then(function(activities) {
-				activities.some(function(activity) {
-					if(activity.label === 'Fernsehen') {
-						var id = activity.id;
-						harmonyClient.startActivity(id);
-						harmonyClient.end();
-						return true;
-					}
-
-					return false;
-				});
+					return harmonyClient.getActivities()
+						.then(function(activities) {
+							// Get an activity with the name "Fernsehen" and trigger it:
+							var watchTvActivity = activities
+								.filter(function(activity) { return activity.label.toLowerCase() === 'fernsehen'; })
+								.pop();
+							
+							if(watchTvActivity) {
+								return harmonyClient.startActivity(watchTvActivity.id);
+							} else {
+								throw new Error('Could not find an activity that sounds like Fernsehen :(');
+							}
+						});
+				} else {
+					console.log('Currently on. Turning TV off');
+					
+					return harmonyClient.turnOff();
+				}
+			})
+			.finally(function() {
+				harmonyClient.end();
 			});
-		} else {
-			console.log('Currently on. Turning TV off');
-
-			harmonyClient.turnOff();
-			harmonyClient.end();
-		}
+	})
+	.catch(function(e) {
+		console.log(e);
 	});
-});
