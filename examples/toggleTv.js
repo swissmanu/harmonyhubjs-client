80466cfcb02fbaf8f8ed049e6979be40b3da43cf
Manuel Alabor <manuel@alabor.me>
2015-12-28 19:47:46 +0100

    docs(examples): Bring examples to standard code style

--- examples/toggleTv.js
+++ examples/toggleTv.js
@@ -1,35 +1,35 @@
-var HarmonyHubClient = require('../index');
+var HarmonyHubClient = require('../index')
 
 HarmonyHubClient('192.168.1.11')
-	.then(function(harmonyClient) {
-		return harmonyClient.isOff()
-			.then(function(off) {
-				if(off) {
-					console.log('Currently off. Turning TV on.');
+  .then(function (harmonyClient) {
+    return harmonyClient.isOff()
+      .then(function (off) {
+        if (off) {
+          console.log('Currently off. Turning TV on.')
 
-					return harmonyClient.getActivities()
-						.then(function(activities) {
-							// Get an activity with the name "Fernsehen" and trigger it:
-							var watchTvActivity = activities
-								.filter(function(activity) { return activity.label.toLowerCase() === 'fernsehen'; })
-								.pop();
-							
-							if(watchTvActivity) {
-								return harmonyClient.startActivity(watchTvActivity.id);
-							} else {
-								throw new Error('Could not find an activity that sounds like Fernsehen :(');
-							}
-						});
-				} else {
-					console.log('Currently on. Turning TV off');
-					
-					return harmonyClient.turnOff();
-				}
-			})
-			.finally(function() {
-				harmonyClient.end();
-			});
-	})
-	.catch(function(e) {
-		console.log(e);
-	});
+          return harmonyClient.getActivities()
+            .then(function (activities) {
+              // Get an activity with the name "Fernsehen" and trigger it:
+              var watchTvActivity = activities
+                .filter(function (activity) { return activity.label.toLowerCase() === 'fernsehen' })
+                .pop()
+
+              if (watchTvActivity) {
+                return harmonyClient.startActivity(watchTvActivity.id)
+              } else {
+                throw new Error('Could not find an activity that sounds like Fernsehen :(')
+              }
+            })
+        } else {
+          console.log('Currently on. Turning TV off')
+
+          return harmonyClient.turnOff()
+        }
+      })
+      .finally(function () {
+        harmonyClient.end()
+      })
+  })
+  .catch(function (e) {
+    console.log(e)
+  })
